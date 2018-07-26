295ae0005a25c7f72f60357d64932a61f0dc5ac9
Jonas Rodon <jonasrodon@gmail.com>
2018-07-26 16:02:14 +0200

    RestServer -usuario fix

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,6 @@ let urlDB
 if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-  urlDB = 'mongodb://cafe-user:cafe-123456>@ds147181.mlab.com:47181/cafe'
+  urlDB = 'mongodb://cafe-user:cafe-123456@ds147181.mlab.com:47181/cafe'
 }
 process.env.URLDB = urlDB
