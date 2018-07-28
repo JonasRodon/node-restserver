5938a3bb6c83a14b550e86531ac68dba5a7d6827
Jonas Rodon <jonasrodon@gmail.com>
2018-07-29 01:51:42 +0200

    Login normal

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 require('./config/config')
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
+const app = express()
 const bodyParser = require('body-parser')
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
-app.use(require('./routes/usuario'))
+// Configuración global de rutas
+app.use(require('./routes/index'))
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, (err, res) => {
   if (err) throw err
   console.log('Base de datos Online')
